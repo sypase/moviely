7489aa46fe91e2f9c5a8a5bd647290cd45852d6d
sunabranjitkar <sunabranjit24@gmail.com>
2021-01-07 10:05:42 +0545

    Added Rentals

--- models/rental.js
+++ models/rental.js
@@ -11,7 +11,59 @@ const Rental = mongoose.model(
           minlength: 5,
           maxlength: 50,
         },
+        isGold: {
+          type: Boolean,
+          default: false,
+        },
+        phone: {
+          type: String,
+          required: true,
+          minlength: 5,
+          maxlength: 50,
+        },
       }),
+      required: true,
+    },
+    movie: {
+      type: new mongoose.Schema({
+        title: {
+          type: String,
+          required: true,
+          trim: true,
+          minlength: 5,
+          maxlength: 255,
+        },
+        dailyRentalRate: {
+          type: Number,
+          required: true,
+          min: 0,
+          max: 255,
+        },
+      }),
+      required: true,
+    },
+    dateOut: {
+      type: Date,
+      required: true,
+      default: Date.now,
+    },
+    dateReturned: {
+      type: Date,
+    },
+    rentalFee: {
+      type: Number,
+      min: 0,
     },
   })
 );
+function ValidateRental(rental) {
+  const schema = {
+    customerId: Joi.string().required(),
+    movieId: Joi.string().required(),
+  };
+  return Joi.validate(rental, schema);
+}
+
+exports.Rental = Rental;
+exports.validate = ValidateRental;
+  
---
+++ routes/rentals.js
@@ -0,0 +1,40 @@
+const { Rental, validate } = require('../models/rental');
+const { Movie } = require('../models/movie');
+const { Customer } = require('../models/customer');
+const mongoose = require('mongoose');
+const express = require('express');
+const router = express.Router();
+///////////////////////////////////////
+
+router.get('/', async (req, res) => {
+  const rentals = await Rental.find().sort('-dateOut');
+  res.send(rentals);
+});
+
+router.post('/', async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+  const customer = await Customer.findById(req.body.customerId);
+  if (!customer) return res.status(400).send('Invaid customer.');
+  const movie = await Movie.findById(req.body.movieId);
+  if (!movie) return res.status(400).send('Invalid movie.');
+
+  if (movie.numberInStock === 0)
+    return res.status(400).send('Movie Out of Stock');
+  let rental = new Rental({
+    customer: {
+      _id: customer._id,
+      name: customer.name,
+      phone: customer.phone,
+    },
+    movie: {
+      _id: movie._id,
+      title: movie.title,
+      dailyRentalRate: movie.dailyRentalRate,
+    },
+  });
+  rental = await rental.save();
+  movie.numberInStock--;
+  movie.save();
+  res.send(rental);
+});
